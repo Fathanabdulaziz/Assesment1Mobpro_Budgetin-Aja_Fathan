a3653b766c7cc5976c908453cb7956d48c56d1a1
Fathan Abdul Aziz <fathanabdulaziz528@gmail.com>
2025-03-24 14:40:40 +0700

    menambahkan isi dari Strings.xml dan merapihkan Text pada ScreenContent

--- app/src/main/java/com/fathan0041/budgetin_aja_fathan/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fathan0041/budgetin_aja_fathan/ui/screen/MainScreen.kt
@@ -1,6 +1,9 @@
 package com.fathan0041.budgetin_aja_fathan.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fathan0041.budgetin_aja_fathan.R
 import com.fathan0041.budgetin_aja_fathan.ui.theme.BudgetinAja_FathanTheme
 
@@ -36,10 +40,16 @@ fun MainScreen(){
 }
 @Composable
 fun ScreenContent (modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android!!!",
-        modifier = modifier
-    )
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ){
+        Text(
+            text = stringResource(R.string.desc_app),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
