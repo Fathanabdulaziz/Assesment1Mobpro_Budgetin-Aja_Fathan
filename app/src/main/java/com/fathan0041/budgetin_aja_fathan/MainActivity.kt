33c148542bd5761ccc5bef70d3f09b6dc3e6a540
Fathan Abdul Aziz <fathanabdulaziz528@gmail.com>
2025-03-24 12:53:20 +0700

    membuuat package folder screen, menambahkan TopAppBar, dan menambahkan ScreenContent

--- app/src/main/java/com/fathan0041/budgetin_aja_fathan/MainActivity.kt
+++ app/src/main/java/com/fathan0041/budgetin_aja_fathan/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.fathan0041.budgetin_aja_fathan.ui.screen.MainScreen
 import com.fathan0041.budgetin_aja_fathan.ui.theme.BudgetinAja_FathanTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BudgetinAja_FathanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BudgetinAja_FathanTheme {
-        Greeting("Android")
-    }
-}
