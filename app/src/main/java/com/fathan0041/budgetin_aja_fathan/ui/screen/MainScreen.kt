2590d6e31f2aeec45d011be40e6714ba649f00ed
Fathan Abdul Aziz <fathanabdulaziz528@gmail.com>
2025-03-24 19:37:32 +0700

    menambahkan isi dari Strings.xml dan membuat outlinedTextField

--- app/src/main/java/com/fathan0041/budgetin_aja_fathan/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fathan0041/budgetin_aja_fathan/ui/screen/MainScreen.kt
@@ -1,19 +1,29 @@
 package com.fathan0041.budgetin_aja_fathan.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fathan0041.budgetin_aja_fathan.R
@@ -40,12 +50,51 @@ fun MainScreen(){
 }
 @Composable
 fun ScreenContent (modifier: Modifier = Modifier){
+    var name by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("") }
     Column (
-        modifier = modifier.fillMaxSize().padding(16.dp)
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
             text = stringResource(R.string.desc_app),
             style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it},
+            label = {
+                Text(
+                    text = stringResource(R.string.label)
+                )
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = price,
+            onValueChange = { price = it},
+            label = {
+                Text(
+                    text = stringResource(R.string.price)
+                )
+            },
+            leadingIcon = {
+                Text(
+                    text = "Rp"
+                )
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             modifier = Modifier.fillMaxWidth()
         )
     }
